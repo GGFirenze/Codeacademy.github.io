f515717753e0d7f71fc30c7dafbbfd730a57cc26
Giuliano Giannini <112504715+GGFirenze@users.noreply.github.com>
2023-04-21 21:18:00 +0100

    Create C++ Exercise.cpp

---
+++ C++ Exercise.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+  // I play rumba on my guitar.
+  std::cout<<" GGG  II U    U LL     II\n";
+  std::cout<<"G   G II U    U LL     II\n";
+  std::cout<<"G     II U    U LL     II\n";
+  std::cout<<"GGGGG II U    U LL     II\n";
+  std::cout<<"G   G II U    U LL     II\n";
+  std::cout<<"G   G II U    U LL     II\n";
+  std::cout<<" GGG  II  UUUU  LLLLLL II\n";
+
+}
