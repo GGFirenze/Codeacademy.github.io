c0b2db67512a8a8ed21de90736626aad238daf35
Giuliano Giannini <112504715+GGFirenze@users.noreply.github.com>
2023-04-23 14:57:20 +0100

    Update Enter_PIN.cpp

--- Enter_PIN.cpp
+++ Enter_PIN.cpp
@@ -9,27 +9,24 @@ int pin = 0;
   
   std::cout << "Enter your PIN: ";
   std::cin >> pin;
-
-  tries++;
+ tries++;
 
   while (pin != 1234 && tries < 3) {
     
     std::cout << "Enter your PIN: ";
     std::cin >> pin;
-    std::cout << "Invalid PIN\n";
     tries++;
     
   }
-
-  
-  if (pin == 1234) {
+ 
+ if (pin == 1234) {
     
     std::cout << "PIN accepted!\n";
     std::cout << "You now have access.\n"; 
     
   }
 
-  if (tries == 3) {
+  if (pin != 1234 && tries == 3) {
     std::cout << "3 failed attempts! Card blocked! Call Admin to unblock.\n";
     std::cout << "Ora son cazzi tua!\n";
   }
