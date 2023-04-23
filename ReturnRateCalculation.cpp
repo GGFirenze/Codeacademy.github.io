314e2b3e7440af38abc803de048129d02583c211
Giuliano Giannini <112504715+GGFirenze@users.noreply.github.com>
2023-04-23 13:08:45 +0100

    Create ReturnRateCalculation.cpp

---
+++ ReturnRateCalculation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main() {
+  int TotalParticipants, Surveys;
+  double ReturnRate = (Surveys / TotalParticipants) * 100;
+  std::cout << "Please, type the total number of participants: ";
+  std::cin >> TotalParticipants;
+  std::cout << "Please, type the total number of surveys: ";
+  std::cin >> Surveys;
+  std::cout << "Based on the data provided, your return rate percentage is: " << ReturnRate << ".\n";
+  
+  if (ReturnRate >= 20.0) {
+  std::cout << "You rock! Keep up with the good work!\n";
+} else if (ReturnRate > 19.0 && ReturnRate < 20.0){
+  std::cout << "Come one, dude! Almost there, don't give up now!";
+} else {
+  std::cout << "Sell this survey as there is no tomorrow! Sooner or later this numbers will go up! Also, your line managers suck!";
+}
+}
