229d9f20915931d17b7519adf759b7ab1023bd52
Giuliano Giannini <112504715+GGFirenze@users.noreply.github.com>
2024-04-08 01:35:36 +0100

    Update kelvinvar.js

--- kelvinvar.js
+++ kelvinvar.js
@@ -1,9 +1,13 @@
 //variable with constant value
-const kelvin = 293;
+const kelvin = 0;
 //celsius is 273 degrees less than kelvin
-var celsius = kelvin - 273;
+const celsius = kelvin - 273;
 //fahrenheit is celsius times 9/5 + 32
-var fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9/5) + 32;
 //to round the result I should use var = Math.floor(var);
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+let newton = celsius*(33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
